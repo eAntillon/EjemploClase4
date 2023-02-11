0d0cac5f322c1a2efecfe3dbc7d4be1516ba9ed4
Erick Antillon <jpj007.ek@gmail.com>
2023-02-11 11:59:17 -0600

    completado ejemplo clase 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("cambios nuevos");
+        Animal an = new Animal("pato", 4);
+
+        an.setNombre("perro");
+        an.comunicarse();
+        System.out.println(an.getNombre());
+
+        Gato gato1 = new Gato("michi", 2, "angora");
+
+        gato1.setNombre("michito");
+        gato1.comunicarse();
+        System.out.println(gato1.getRaza());
+
     }
 }
